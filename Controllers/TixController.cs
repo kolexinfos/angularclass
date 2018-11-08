004a9de27eb6eda077192438e5a6612fc6e94a7e
Kolade Kehinde <kokehind@microsoft.com>
2018-11-08 14:46:51 +0100

    Initial Commit

---
+++ Controllers/TixController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using BuyTixApi.Business;
+using BuyTixApi.Models.Movies;
+using BuyTixApi.Models.Purchase;
+using BuyTixApi.Models.Seats;
+
+namespace BuyTixApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TixController : Controller
+    {
+        [HttpGet]
+        [Route("GetMovies")]
+        [ProducesResponseType(typeof(List<ActiveMovie>), 200)]
+        public IActionResult GetAllActiveMovies()
+        {
+            return Ok(Business.Movies.GetAllActiveMovies());
+        }
+
+        [HttpGet]
+        [Route("GetSeatingPlan/{ActiveMovieId}")]
+        [ProducesResponseType(typeof(SeatingPlan), 200)]
+        public IActionResult GetActiveMovieSeatingPlan(int ActiveMovieId)
+        {
+            return Ok(Business.Movies.GetActiveMovieSeatingPlan(ActiveMovieId));
+        }
+
+        [HttpPost]
+        [Route("PurchaseTickets")]
+        [ProducesResponseType(typeof(string), 200)]
+        public IActionResult PurchaseTickets([FromBody]Order order) 
+        {
+           return Ok(Business.Movies.PurchaseTickets(order));
+        }
+    }
+}
---
+++ Models/Movies/Movie.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BuyTixApi.Models.Movies
+{
+    public class Movie
+    {
+        public long MovieId;
+        public string MovieName;
+        public string MovieDescription;
+    }
+}
